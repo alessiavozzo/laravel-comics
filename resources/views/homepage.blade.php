df7e8bfd4615bde054468a8bc36ba1af98e39f21
alessiavozzo <a.vozzo95@gmail.com>
2024-05-07 17:49:35 +0200

    add banner

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -22,9 +22,52 @@
 
         </div>
 
-
+        <div class="load pt-4 pb-3 text-center">
+            <button type="button" class="btn btn-primary text-white fw-bold px-5 rounded-0">
+                LOAD MORE
+            </button>
+        </div>
     </div>
 
+</section>
+
+<section id="buy-banner" class="bg-primary py-4">
+    <div class="container">
+        <div class="row row-cols-1 row-cols-md-5 gx-4">
+
+            <div class="col">
+                <div class="product d-flex align-items-center gap-2">
+                    <img src="{{Vite::asset('resources/img/buy-comics-digital-comics.png')}}" alt="">
+                    <div class="text text-uppercase text-white">Digital Comics</div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="product d-flex align-items-center gap-2">
+                    <img src="{{Vite::asset('resources/img/buy-comics-merchandise.png')}}" alt="">
+                    <div class="text text-uppercase text-white">Dc merchandise</div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="product d-flex align-items-center gap-2">
+                    <img src="{{Vite::asset('resources/img/buy-comics-subscriptions.png')}}" alt="">
+                    <div class="text text-uppercase text-white">subscription</div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="product d-flex align-items-center gap-2">
+                    <img src="{{Vite::asset('resources/img/buy-comics-shop-locator.png')}}" alt="">
+                    <div class="text text-uppercase text-white">comic shop locator</div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="product d-flex align-items-center gap-2">
+                    <img src="{{Vite::asset('resources/img/buy-dc-power-visa.png')}}" alt="">
+                    <div class="text text-uppercase text-white">dc power visa</div>
+                </div>
+            </div>
+
+        </div>
+    </div>
 </section>
 
 @endsection
